5f73f1eabb27ecaa34b60c0f5e8620507d527cfb
Hamza <hamzamunawar345@gmail.com>
2023-11-27 11:47:10 +0500

    11:46am

--- src/app/monthly-transaction/monthly-transaction.component.ts
+++ src/app/monthly-transaction/monthly-transaction.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MonthlyTransactionComponent implements OnInit {
   userName: string= "Hamza Munawar";
+  
   constructor() { }
 
   ngOnInit(): void {
